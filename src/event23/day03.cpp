e3a7c76f0c486e88b97fcb665f257ed5cc95f143
canozo <canwoahzo@gmail.com>
2023-12-17 18:20:28 -0600

    2023 day 3

--- src/event23/day03.cpp
+++ src/event23/day03.cpp
@@ -1,5 +1,10 @@
+#include <cctype>
 #include <fstream>
+#include <map>
+#include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 #include <aoc.h>
 
 namespace event23::day03 {
@@ -7,18 +12,121 @@ namespace event23::day03 {
 constexpr int EVENT = 2023;
 constexpr int DAY = 3;
 
+constexpr int npos = -1;
+
+struct PartNumber {
+  int number;
+  bool seen;
+};
+
+const std::vector<std::pair<int, int>> BORDERS {
+  std::make_pair(-1, -1),
+  std::make_pair(-1,  0),
+  std::make_pair(-1,  1),
+
+  std::make_pair(0, -1),
+  std::make_pair(0,  1),
+
+  std::make_pair(1, -1),
+  std::make_pair(1,  0),
+  std::make_pair(1,  1),
+};
+
 aoc::Answer solve() {
   int part1 = 0;
   int part2 = 0;
 
-  std::ifstream input("inputs/event23/day03/example.txt");
+  std::ifstream input("inputs/event23/day03/input.txt");
 
   if (!input) {
     return aoc::Answer { EVENT, DAY, "", "" };
   }
 
+  std::vector<std::string> schematic;
+
   for (std::string line; std::getline(input, line);) {
-    // TODO
+    schematic.push_back(line);
+  }
+
+  std::vector<std::pair<int, int>> symbols;
+  std::map<std::pair<int, int>, std::shared_ptr<PartNumber>> part_numbers;
+
+  for (int lidx = 0; lidx < schematic.size(); lidx++) {
+    const std::string &line = schematic.at(lidx);
+
+    std::shared_ptr<PartNumber> part_number;
+    int num_begin = npos;
+
+    for (int cidx = 0; cidx < line.length(); cidx++) {
+      const char curr = line[cidx];
+      const bool is_digit = std::isdigit(curr);
+
+      if (is_digit && num_begin == npos) {
+        part_number = std::make_shared<PartNumber>();
+        part_numbers.insert({ std::make_pair(lidx, cidx), part_number });
+
+        num_begin = cidx;
+        continue;
+      }
+
+      if (is_digit) {
+        part_numbers.insert({ std::make_pair(lidx, cidx), part_number });
+        continue;
+      }
+
+      if (num_begin != npos) {
+        int num = std::stoi(line.substr(num_begin, cidx - num_begin));
+
+        part_number->number = num;
+        part_number->seen = false;
+
+        num_begin = npos;
+      }
+
+      if (curr != '.') {
+        symbols.push_back(std::make_pair(lidx, cidx));
+      }
+    }
+
+    if (num_begin != npos) {
+      int num = std::stoi(line.substr(num_begin, line.length() - num_begin));
+
+      part_number->number = num;
+      part_number->seen = false;
+    }
+  }
+
+  for (const auto &[sline, scol] : symbols) {
+    const bool gear_symbol = schematic[sline][scol] == '*';
+    int part_number_count = 0;
+    int gear_ratio = 1;
+
+    for (const auto &[bline, bcol] : BORDERS) {
+      const auto pair = std::make_pair(sline + bline, scol + bcol);
+      const auto it = part_numbers.find(pair);
+
+      if (it == part_numbers.end()) {
+        continue;
+      }
+
+      const std::shared_ptr<PartNumber> part_number = it->second;
+
+      if (part_number->seen) {
+        continue;
+      }
+
+      part1 += part_number->seen ? 0 : part_number->number;
+      part_number->seen = true;
+
+      if (gear_symbol) {
+        part_number_count++;
+        gear_ratio *= part_number->number;
+      }
+    }
+
+    if (part_number_count == 2) {
+      part2 += gear_ratio;
+    }
   }
 
   return aoc::Answer {
