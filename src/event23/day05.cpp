a00c4a2caaa1d66a78973549c48bd23d9738740f
canozo <canwoahzo@gmail.com>
2023-12-24 03:13:54 -0600

    failed part 2 attempt

--- src/event23/day05.cpp
+++ src/event23/day05.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <cctype>
 #include <fstream>
+#include <iterator>
 #include <string>
 #include <vector>
 #include <aoc.h>
@@ -17,6 +18,13 @@ struct Mapping {
   unsigned long dest;
   unsigned long source;
   unsigned long range;
+};
+
+struct SeedGroup {
+  unsigned long fr;
+  unsigned long to;
+  bool converted = false;
+  bool prune = false;
 };
 
 Mapping parseMapping(std::string &line) {
@@ -54,12 +62,80 @@ Mapping parseMapping(std::string &line) {
   return mapping;
 }
 
+void aggregateSeedGroups(std::vector<SeedGroup> &seeds) {
+  // seeds must be sorted
+  std::sort(
+    seeds.begin(),
+    seeds.end(),
+    [](const SeedGroup &l, const SeedGroup &r) {
+      return l.fr < r.fr;
+    });
+
+  std::vector<SeedGroup> append;
+  unsigned long ceiling = 0;
+
+  int l = 0;
+  int r = 1;
+
+  while (l < seeds.size() && r < seeds.size()) {
+    SeedGroup &start = seeds.at(l);
+    SeedGroup &next = seeds.at(r);
+
+    if (start.converted != next.converted) {
+      l++;
+
+      if (l == r) {
+        r++;
+      }
+
+      continue;
+    }
+
+    if (ceiling && ceiling >= next.fr) {
+      next.prune = true;
+      ceiling = std::max(ceiling, next.to);
+      r++;
+      continue;
+    }
+
+    if (start.to >= next.fr) {
+      start.prune = next.prune = true;
+      ceiling = std::max(start.to, next.to);
+      r++;
+      continue;
+    }
+
+    if (ceiling) {
+      append.push_back(SeedGroup { start.fr, ceiling, start.converted });
+      ceiling = 0;
+      l = r;
+      r++;
+    } else {
+      l++;
+      r++;
+    }
+  }
+
+  if (ceiling) {
+    const SeedGroup start = seeds.at(l);
+    append.push_back(SeedGroup { start.fr, ceiling, start.converted });
+  }
+
+  seeds.erase(std::remove_if(
+    seeds.begin(),
+    seeds.end(),
+    [](const SeedGroup &sg) { return sg.prune; }),
+    seeds.end());
+
+  seeds.insert(seeds.end(), append.begin(), append.end());
+}
+
 void convertSeeds(
   std::vector<unsigned long> &seeds,
   std::vector<bool> &converted,
-  std::string &line
+  const Mapping &line
 ) {
-  const auto &[dest, source, range] = parseMapping(line);
+  const auto &[dest, source, range] = line;
 
   for (int idx = 0; idx < seeds.size(); idx++) {
     if (converted.at(idx)) {
@@ -75,9 +151,65 @@ void convertSeeds(
   }
 }
 
+void convertSeedGroups(std::vector<SeedGroup> &seeds, const Mapping &line) {
+  const auto &[dest, source, range] = line;
+
+  std::vector<SeedGroup> append;
+
+  for (SeedGroup &seed : seeds) {
+    if (seed.converted) {
+      continue;
+    }
+
+    const unsigned long map_fr = source;
+    const unsigned long map_to = source + range;
+
+    if (seed.fr > map_to || map_fr > seed.to) {
+      continue;
+    }
+
+    const unsigned long diff = dest - source;
+
+    // whole seed group is contained on the range
+    if (map_fr <= seed.fr && map_to >= seed.to) {
+      seed.fr += diff;
+      seed.to += diff;
+      seed.converted = true;
+      continue;
+    }
+
+    const unsigned long og_fr = std::max(seed.fr, map_fr);
+    const unsigned long og_to = std::min(seed.to, map_to);
+
+    // either the end or the beginning of the seed group is in the range
+    if (map_fr <= seed.fr) {
+      append.push_back(SeedGroup { map_to + 1, seed.to });
+    } else if (map_to >= seed.to) {
+      append.push_back(SeedGroup { seed.fr, map_fr - 1 });
+    }
+
+    // a spot in the middle of the seed group is part of the range
+    if (map_fr > seed.fr && map_to < seed.to) {
+      append.push_back(SeedGroup { seed.fr + diff, map_fr + diff });
+      append.push_back(SeedGroup { map_to + diff, seed.to + diff });
+    }
+
+    // move the intersection to the mapped spot
+    seed.fr = og_fr + diff;
+    seed.to = og_to + diff;
+    seed.converted = true;
+  }
+
+  // append new seeds to main vector
+  seeds.insert(seeds.end(), append.begin(), append.end());
+
+  // sort and aggregate groups
+  aggregateSeedGroups(seeds);
+}
+
 aoc::Answer solve(const std::string &filename = "input.txt") {
   unsigned long part1 = 0;
-  int part2 = 0;
+  unsigned long part2 = 0;
 
   std::ifstream input("inputs/event23/day05/" + filename);
 
@@ -86,6 +218,7 @@ aoc::Answer solve(const std::string &filename = "input.txt") {
   }
 
   std::vector<unsigned long> seeds;
+  std::vector<SeedGroup> seedGroups;
 
   std::string first_line;
   std::getline(input, first_line);
@@ -93,6 +226,7 @@ aoc::Answer solve(const std::string &filename = "input.txt") {
 
   int pos = 0;
 
+  // parse the seeds for part 1
   for (int idx = seeds_start; idx < first_line.length(); idx++) {
     const char curr = first_line[idx];
 
@@ -110,26 +244,56 @@ aoc::Answer solve(const std::string &filename = "input.txt") {
     }
   }
 
-  pos = 0;
-
   std::vector<bool> converted(seeds.size(), false);
 
+  // parse the seeds groups for part 2
+  for (auto it = seeds.begin(); it != seeds.end(); std::advance(it, 2)) {
+    const unsigned long start = *it;
+
+    auto dupe = it;
+    std::advance(dupe, 1);
+    const unsigned long range = *dupe;
+
+    seedGroups.push_back(SeedGroup { start, start + range });
+  }
+
+  // iterate through almanac to solve puzzle
   for (std::string line; std::getline(input, line);) {
     if (line.empty()) {
       continue;
     }
 
     if (!std::isdigit(line[0])) {
+      // reset part 1 flags
       std::fill(converted.begin(), converted.end(), false);
+
+      // reset part 2 flags
+      for (SeedGroup &seed : seedGroups) {
+        seed.converted = false;
+      }
+
+      // try to aggregate values
+      aggregateSeedGroups(seedGroups);
     } else {
-      convertSeeds(seeds, converted, line);
+      const Mapping m = parseMapping(line);
+      convertSeeds(seeds, converted, m);
+      convertSeedGroups(seedGroups, m);
     }
   }
 
-  auto min_value = std::min_element(seeds.begin(), seeds.end());
+  auto min_val_p1 = std::min_element(seeds.begin(), seeds.end());
 
-  if (min_value != seeds.end()) {
-    part1 = *min_value;
+  if (min_val_p1 != seeds.end()) {
+    part1 = *min_val_p1;
+  }
+
+  auto min_val_p2 = std::min_element(seedGroups.begin(), seedGroups.end(),
+    [](const SeedGroup &l, const SeedGroup &r) {
+      return l.fr < r.fr;
+    });
+
+  if (min_val_p2 != seedGroups.end()) {
+    part2 = (*min_val_p2).fr;
   }
 
   return aoc::Answer {
